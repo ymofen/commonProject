893092ea02a58dd50d2e29bfa10978d9faadcfef
ymofen <185511468@qq.com>
2015-06-01 16:40:56 +0800

    * 蓝牙代理

---
+++ c#/COMProxy/COMProxy/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace COMProxy
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOpenCOM1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.sPort01 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cbbCOM02 = new System.Windows.Forms.ComboBox();
+            this.sPort02 = new System.IO.Ports.SerialPort(this.components);
+            this.txtPort2 = new System.Windows.Forms.TextBox();
+            this.btnOpenCOM2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lstFilter = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCOMDATA_01 = new System.Windows.Forms.TextBox();
+            this.SendCOM01 = new System.Windows.Forms.Button();
+            this.btnSendCOM01Hex = new System.Windows.Forms.Button();
+            this.btnHexToString = new System.Windows.Forms.Button();
+            this.chkCOM02RecordToFile = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnOpenCOM1
+            // 
+            this.btnOpenCOM1.Location = new System.Drawing.Point(139, 20);
+            this.btnOpenCOM1.Name = "btnOpenCOM1";
+            this.btnOpenCOM1.Size = new System.Drawing.Size(87, 23);
+            this.btnOpenCOM1.TabIndex = 0;
+            this.btnOpenCOM1.Text = "打开COM接口";
+            this.btnOpenCOM1.UseVisualStyleBackColor = true;
+            this.btnOpenCOM1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // sPort01
+            // 
+            this.sPort01.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sPort01_DataReceived);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 84);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(599, 213);
+            this.textBox1.TabIndex = 2;
+            // 
+            // cbbCOM02
+            // 
+            this.cbbCOM02.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbbCOM02.FormattingEnabled = true;
+            this.cbbCOM02.Location = new System.Drawing.Point(15, 323);
+            this.cbbCOM02.Name = "cbbCOM02";
+            this.cbbCOM02.Size = new System.Drawing.Size(121, 20);
+            this.cbbCOM02.TabIndex = 3;
+            // 
+            // sPort02
+            // 
+            this.sPort02.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sPort02_DataReceived);
+            // 
+            // txtPort2
+            // 
+            this.txtPort2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPort2.Location = new System.Drawing.Point(12, 350);
+            this.txtPort2.Multiline = true;
+            this.txtPort2.Name = "txtPort2";
+            this.txtPort2.Size = new System.Drawing.Size(760, 164);
+            this.txtPort2.TabIndex = 4;
+            // 
+            // btnOpenCOM2
+            // 
+            this.btnOpenCOM2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnOpenCOM2.Location = new System.Drawing.Point(151, 321);
+            this.btnOpenCOM2.Name = "btnOpenCOM2";
+            this.btnOpenCOM2.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenCOM2.TabIndex = 5;
+            this.btnOpenCOM2.Text = "打开COM接口";
+            this.btnOpenCOM2.UseVisualStyleBackColor = true;
+            this.btnOpenCOM2.Click += new System.EventHandler(this.btnOpenCOM2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(642, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "测试";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(621, 22);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(109, 23);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "关闭所有端口";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lstFilter
+            // 
+            this.lstFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFilter.FormattingEnabled = true;
+            this.lstFilter.Items.AddRange(new object[] {
+            "$GPGGA,",
+            "$GNGST,",
+            "$GPZDA,",
+            "$GNGSA,",
+            "$GPGSV,",
+            "$GLGSV,",
+            "$GBDGSV,",
+            "$PTNL,BPQ,",
+            "$$"});
+            this.lstFilter.Location = new System.Drawing.Point(617, 84);
+            this.lstFilter.Name = "lstFilter";
+            this.lstFilter.Size = new System.Drawing.Size(155, 212);
+            this.lstFilter.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(619, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "过滤项目";
+            this.label1.DoubleClick += new System.EventHandler(this.label1_DoubleClick);
+            // 
+            // txtCOMDATA_01
+            // 
+            this.txtCOMDATA_01.Location = new System.Drawing.Point(12, 57);
+            this.txtCOMDATA_01.Name = "txtCOMDATA_01";
+            this.txtCOMDATA_01.Size = new System.Drawing.Size(427, 21);
+            this.txtCOMDATA_01.TabIndex = 10;
+            // 
+            // SendCOM01
+            // 
+            this.SendCOM01.Location = new System.Drawing.Point(445, 55);
+            this.SendCOM01.Name = "SendCOM01";
+            this.SendCOM01.Size = new System.Drawing.Size(86, 23);
+            this.SendCOM01.TabIndex = 11;
+            this.SendCOM01.Text = "发送到端口1";
+            this.SendCOM01.UseVisualStyleBackColor = true;
+            this.SendCOM01.Click += new System.EventHandler(this.SendCOM01_Click);
+            // 
+            // btnSendCOM01Hex
+            // 
+            this.btnSendCOM01Hex.Location = new System.Drawing.Point(536, 55);
+            this.btnSendCOM01Hex.Name = "btnSendCOM01Hex";
+            this.btnSendCOM01Hex.Size = new System.Drawing.Size(75, 23);
+            this.btnSendCOM01Hex.TabIndex = 12;
+            this.btnSendCOM01Hex.Text = "Hex发送";
+            this.btnSendCOM01Hex.UseVisualStyleBackColor = true;
+            this.btnSendCOM01Hex.Click += new System.EventHandler(this.btnSendCOM01Hex_Click);
+            // 
+            // btnHexToString
+            // 
+            this.btnHexToString.Location = new System.Drawing.Point(445, 26);
+            this.btnHexToString.Name = "btnHexToString";
+            this.btnHexToString.Size = new System.Drawing.Size(86, 23);
+            this.btnHexToString.TabIndex = 13;
+            this.btnHexToString.Text = "HexToString";
+            this.btnHexToString.UseVisualStyleBackColor = true;
+            this.btnHexToString.Click += new System.EventHandler(this.btnHexToString_Click);
+            // 
+            // chkCOM02RecordToFile
+            // 
+            this.chkCOM02RecordToFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkCOM02RecordToFile.AutoSize = true;
+            this.chkCOM02RecordToFile.Location = new System.Drawing.Point(244, 325);
+            this.chkCOM02RecordToFile.Name = "chkCOM02RecordToFile";
+            this.chkCOM02RecordToFile.Size = new System.Drawing.Size(132, 16);
+            this.chkCOM02RecordToFile.TabIndex = 14;
+            this.chkCOM02RecordToFile.Text = "接收数据记录到文件";
+            this.chkCOM02RecordToFile.UseVisualStyleBackColor = true;
+            this.chkCOM02RecordToFile.CheckedChanged += new System.EventHandler(this.chkCOM02RecordToFile_CheckedChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 526);
+            this.Controls.Add(this.chkCOM02RecordToFile);
+            this.Controls.Add(this.btnHexToString);
+            this.Controls.Add(this.btnSendCOM01Hex);
+            this.Controls.Add(this.SendCOM01);
+            this.Controls.Add(this.txtCOMDATA_01);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstFilter);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnOpenCOM2);
+            this.Controls.Add(this.txtPort2);
+            this.Controls.Add(this.cbbCOM02);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnOpenCOM1);
+            this.Name = "frmMain";
+            this.Text = "COM端口转发";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenCOM1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort sPort01;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cbbCOM02;
+        private System.IO.Ports.SerialPort sPort02;
+        private System.Windows.Forms.TextBox txtPort2;
+        private System.Windows.Forms.Button btnOpenCOM2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckedListBox lstFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCOMDATA_01;
+        private System.Windows.Forms.Button SendCOM01;
+        private System.Windows.Forms.Button btnSendCOM01Hex;
+        private System.Windows.Forms.Button btnHexToString;
+        private System.Windows.Forms.CheckBox chkCOM02RecordToFile;
+
+    }
+}
+
---
+++ c#/COMProxy/COMProxy/Form1.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Globalization;
+
+namespace COMProxy
+{
+    public partial class frmMain : Form
+    {
+        delegate void ThreadWork(String s);
+
+        
+        ///  端口1收到的数据 
+        private MemoryStream data1 = new MemoryStream();
+
+        
+        ///  端口2收到的数据         
+        private MemoryStream data2 = new MemoryStream();
+
+        /// <summary>
+        ///  读取到的数据记录到COM
+        /// </summary>
+        private bool bCOM02Record2File = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public void AppendBuffer(Stream streamData, byte[] buf, int len)
+        {
+            long prePosition = streamData.Position;
+            streamData.Seek(0, SeekOrigin.End);
+            streamData.Write(buf, 0, len);
+            streamData.Position = prePosition;
+        }
+
+        /// <summary>
+        ///   清理当前位置之前的buffer数据
+        /// </summary>
+        /// <param name="streamData"></param>
+        public void ClearReadBuffer(Stream streamData)
+        {
+            byte[] buf = new byte[streamData.Length - streamData.Position];
+            streamData.Read(buf, 0, buf.Length);
+            streamData.SetLength(0);
+            streamData.Write(buf, 0, buf.Length);
+            streamData.Position = 0;
+        }
+
+        public String ReadStringFromStream(Stream streamData)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            byte b = 0;
+            byte CRCount = 0;  // 13 计数 \r
+            byte LFCount = 0;  // 10 计数 \b
+
+            long prePosition = streamData.Position;
+
+            while (streamData.Position < streamData.Length)
+            {
+                b = (byte)streamData.ReadByte();
+
+                if (b == 10)
+                {
+                    LFCount++;
+                    sb.Append((char)b);
+                    if (CRCount == 1) break;
+                }
+                else if (b == 13)
+                {
+                    sb.Append((char)b);
+                    CRCount++;                    
+                }
+                else
+                {
+                    CRCount = 0;
+                    LFCount = 0;
+                    sb.Append((char)b);
+                }
+            }
+
+            if (LFCount >= 1 && CRCount >= 1)
+            {  // 读取完成
+                return sb.ToString();
+            }
+            else
+            {
+                // 没有完整的字符串
+                streamData.Position = prePosition;
+                return null;
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            cbbCOM02.Items.Clear();
+            String[] Portname = SerialPort.GetPortNames();
+         
+
+          //create a loop for each string in SerialPort.GetPortNames
+           foreach (string str in Portname)
+           {
+             comboBox1.Items.Add(str);
+             cbbCOM02.Items.Add(str);
+           }
+        }
+
+        private bool CheckIsFilter(String s)
+        {
+            for (int i = 0; i < lstFilter.CheckedItems.Count; i++)
+            {
+                if (s.IndexOf(lstFilter.CheckedItems[i].ToString()) != -1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static String BytesAsHexString(byte[] bytes)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                sb.Append(String.Format("{0:X2} ", b));
+            }
+            return sb.ToString();
+        }
+
+        public static byte[] HexStringToBytes(String s)
+        {
+            
+            MemoryStream ms = new MemoryStream();
+            s = s.Replace(" ", "");
+            byte[] sbuf = System.Text.ASCIIEncoding.ASCII.GetBytes(s);
+            String hex = "";
+            for (int i = 0; i<= sbuf.Length-1; i++)
+            {                
+                hex = hex + (char)sbuf[i];
+                if (hex.Length == 2)
+                {
+                    ms.WriteByte(Byte.Parse(hex, NumberStyles.HexNumber));
+                    hex = "";
+                }
+            }
+
+            byte[] rbuf = new byte[ms.Length];
+            ms.Position = 0;
+            ms.Read(rbuf, 0, (int)rbuf.Length);
+            return rbuf;
+
+        }
+
+        private void showCOM01DataRecv(String s)
+        {
+            if (s.Length == 0) { return; }
+            if (!CheckIsFilter(s))
+            {
+                textBox1.Text += System.DateTime.Now.ToString() + ":" +  s.ToString();
+                textBox1.Select(textBox1.Text.Length, 0);
+                textBox1.ScrollToCaret();
+            }
+        }
+
+        private void showCOM02DataRecv(String s)
+        {
+            txtPort2.Text += s.ToString();
+            txtPort2.Text += s.ToString();
+            txtPort2.Select(txtPort2.Text.Length, 0);
+            txtPort2.ScrollToCaret();
+        }
+
+        private void sPort01_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] recvBuf = new byte[1024];
+            int l = 0;
+
+            while (sPort01.BytesToRead > 0)
+            {
+                l = sPort01.Read(recvBuf, 0, 1024);
+                if (l > 0)
+                {
+                    AppendBuffer(data1, recvBuf, l);
+                }
+            }
+
+            while (true)
+            {
+                String s = ReadStringFromStream(data1);
+
+                if (s == null) break;
+
+                if (this.InvokeRequired)
+                {
+                    ThreadWork fc = new ThreadWork(showCOM01DataRecv);
+                    this.Invoke(fc, s);
+                }
+                else
+                {
+                    showCOM01DataRecv(s);
+                }
+
+                lock (sPort02)
+                {
+                    if (sPort02.IsOpen)
+                    {
+                        sPort02.Write(s);
+                    }
+                }           
+            }
+
+            ClearReadBuffer(data1);
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sPort01.BaudRate = 9600;
+                sPort01.PortName = comboBox1.Text;
+                sPort01.StopBits = StopBits.One;
+                sPort01.DataBits = 8;
+                sPort01.Parity = System.IO.Ports.Parity.None;
+                sPort01.Open();
+            }catch(Exception E)
+            {
+                MessageBox.Show(E.Message);
+            }
+            
+        }
+
+        private void sPort02_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            byte[] recvBuf = new byte[1024];
+            int l = 0;
+
+            FileStream fWrite = null;
+
+            if (bCOM02Record2File)
+            {
+                String sFile = this.GetType().Assembly.Location;
+                sFile = System.IO.Path.GetFullPath(sFile) + "COM_01.DAT";
+
+                fWrite = new FileStream(sFile, FileMode.Append);
+            }
+            
+            while (sPort02.BytesToRead > 0)
+            {
+                l = sPort02.Read(recvBuf, 0, 1024);
+                if (l > 0)
+                {
+                    AppendBuffer(data2, recvBuf, l);
+                    if (fWrite!=null)
+                    {
+                        fWrite.Write(recvBuf, 0, l);
+                    }
+                }
+            }
+
+            if (fWrite != null)
+            {
+                fWrite.Close();
+            }
+
+            
+
+            
+
+            while (true)
+            {
+                String s = ReadStringFromStream(data2);
+
+                if (s == null) break;
+
+                if (this.InvokeRequired)
+                {
+                    ThreadWork fc = new ThreadWork(showCOM02DataRecv);
+                    this.Invoke(fc, s);
+                }
+                else
+                {
+                    showCOM02DataRecv(s);
+                }
+
+                lock (sPort01)
+                {
+                    if (sPort01.IsOpen)
+                    {
+                        sPort01.Write(s);
+                    }
+                }
+            }
+
+            ClearReadBuffer(data2);
+
+            //MemoryStream ms = new MemoryStream();
+            //byte b = 0;
+
+            //while (sPort02.BytesToRead > 0)
+            //{
+            //    b = (byte)sPort02.ReadByte();
+            //    ms.WriteByte(b);
+            //}
+
+            //if (ms.Length > 0)
+            //{
+            //    ms.Position = 0;
+            //    byte[] buf = new byte[ms.Length];
+            //    ms.Read(buf, 0, buf.Length);
+
+            //    lock (sPort01)
+            //    {
+            //        if (sPort01.IsOpen)
+            //        {
+            //            sPort01.Write(buf, 0, buf.Length);
+            //        }
+            //    }
+
+
+            //    String s = System.Text.Encoding.Default.GetString(buf);
+
+            //    if (this.InvokeRequired)
+            //    {
+            //        ThreadWork fc = new ThreadWork(showCOM02DataRecv);
+            //        this.Invoke(fc, s);
+            //    }
+            //    else
+            //    {
+            //        showCOM02DataRecv(s);
+            //    }
+            //}
+        }
+
+        private void btnOpenCOM2_Click(object sender, EventArgs e)
+        {
+            sPort02.BaudRate = 9600;
+            sPort02.PortName = cbbCOM02.Text;
+            sPort02.StopBits = StopBits.One;
+            sPort02.DataBits = 8;
+            sPort02.Parity = System.IO.Ports.Parity.None;
+            sPort02.Open();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            String hex = "10";
+
+
+            byte b = Byte.Parse(hex, NumberStyles.HexNumber);
+
+            MessageBox.Show(b.ToString());
+
+            //MemoryStream ms = new MemoryStream();
+            //byte[] buf = new byte[1024];
+            //buf[0] = (byte)'a';
+            //buf[1] = (byte)'b';
+            //buf[2] = (byte)'c';
+
+            //AppendBuffer(ms, buf, 3);
+            //byte b =(byte)ms.ReadByte();
+            //ClearReadBuffer(ms);
+
+            //ms.Read(buf, 0, 1024);
+
+            //buf[0] = 0;
+
+
+
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            sPort01.Close();
+            sPort02.Close();
+        }
+
+        private void SendCOM01_Click(object sender, EventArgs e)
+        {
+            String s = txtCOMDATA_01.Text + (char)13 + (char)10;
+            lock (sPort01)
+            {
+                if (sPort01.IsOpen)
+                {
+                    sPort01.Write(s);
+                }
+            }
+        }
+
+        private void btnSendCOM01Hex_Click(object sender, EventArgs e)
+        {
+            String s = txtCOMDATA_01.Text;
+            byte[] sBuf = HexStringToBytes(s);
+            lock (sPort01)
+            {
+                if (sPort01.IsOpen)
+                {
+                    sPort01.Write(sBuf, 0, sBuf.Length);
+                }
+            }
+        }
+
+        private void btnHexToString_Click(object sender, EventArgs e)
+        {
+            String s = txtCOMDATA_01.Text;
+            byte[] sBuf = HexStringToBytes(s);
+
+            s = System.Text.ASCIIEncoding.ASCII.GetString(sBuf);
+            showCOM01DataRecv(s);
+        }
+
+        private void chkCOM02RecordToFile_CheckedChanged(object sender, EventArgs e)
+        {
+            bCOM02Record2File = chkCOM02RecordToFile.Checked;
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lstFilter.CheckedItems.Count; i++)
+            {
+                lstFilter.SetItemChecked(i, true);
+            }
+        }
+    }
+}
